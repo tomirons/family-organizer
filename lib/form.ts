dfe9b9d18d2ed76b54da173e09367694060097c5
tomirons <tom.irons@hotmail.com>
2025-07-31 17:09:29 -0400

    add toast for errors

--- lib/form.ts
+++ lib/form.ts
@@ -1,13 +1,20 @@
 import { AxiosError } from "axios";
 import { FormikHelpers } from "formik";
 import { get } from "lodash";
+import { toast } from "sonner-native";
 
 export const handleFormValidation = (error: AxiosError, helpers: FormikHelpers<any>) => {
-    if (error.response?.status === 422) {
+    const status = get(error, 'response.status');
+    
+    if (status === 422) {
         const errors: Record<string, string[]> = get(error, 'response.data.errors', {});
 
         Object.keys(errors ?? {}).map((item: string) => {
             helpers.setFieldError(item, errors[item][0]);
         });
-    };
+    } else if (status && (status >= 400 && status < 500)) {
+        toast.error(
+            get(error, 'response.data.message', 'An error occurred') as string
+        );
+    }
 }
