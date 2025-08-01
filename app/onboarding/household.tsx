d4afa3c96284a4ad09a9886ae5377b3f4eb987fc
tomirons <tom.irons@hotmail.com>
2025-08-01 16:21:56 -0400

    more tweaks to onboarding screens

--- app/onboarding/household.tsx
+++ app/onboarding/household.tsx
@@ -2,7 +2,7 @@ import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Button } from "~/components/ui/button";
 import { Text } from "~/components/ui/text";
-import { H1 } from "~/components/ui/typography";
+import { H1, P } from "~/components/ui/typography";
 import { Image } from "expo-image";
 import { Formik } from "formik";
 import * as Yup from 'yup';
@@ -18,51 +18,54 @@ const validationSchema = Yup.object().shape({
 
 export default function OnboardingStepTwo() {
     return (
-        <SafeAreaView className="px-6 flex-1 gap-y-16">
-            <View className="items-center mt-24 gap-y-4">
+        <SafeAreaView className="px-6 flex-1 gap-y-8">
+            <View className="items-center mt-12 gap-y-4">
                 <H1>Name Your Household</H1>
             </View>
 
             <Image
-                style={{ width: '100%', height: 300 }}
+                style={{ width: '100%', height: 200 }}
                 source={require("~/assets/images/onboarding/step-2.png")}
                 contentFit="contain"
             />
 
-            <View className="flex-1 justify-between">
-                <Formik
-                    initialValues={{
-                        name: "My Household",
-                    }}
-                    onSubmit={(values, formikHelpers) => {
-                        axios
-                            .post('/households', values)
-                            .then(() => {
-                                router.push('/onboarding/members');
-                            })
-                            .catch((error) => handleFormValidation(error, formikHelpers));
-                    }}
-                    validationSchema={validationSchema}
-                >
-                    {({ values, handleChange, handleBlur, handleSubmit }) => (
-                        <>
-                            <View className="gap-y-4">
-                                <Text>What&apos;s your household&apos;s nickname? Have fun with it!</Text>
+            <Formik
+                initialValues={{
+                    name: "My Household",
+                }}
+                onSubmit={(values, formikHelpers) => {
+                    axios
+                        .post('/households', values)
+                        .then((r) => {                            
+                            router.navigate({
+                                pathname: '/onboarding/members',
+                                params: { household: r.data.data.id }
+                            });
+                        })
+                        .catch((error) => handleFormValidation(error, formikHelpers));
+                }}
+                validationSchema={validationSchema}
+            >
+                {({ values, handleChange, handleBlur, handleSubmit }) => (
+                    <View className="flex-1 justify-between">
+                        <View className="gap-y-4">
+                            <P className="text-lg">
+                                Every great household needs a iconic name! Is yours <Text className="text-lg font-semibold">The Glitter Palace</Text> or <Text className="text-lg font-semibold">Couch Potato HQ</Text>? Give it a title that screams <Text className="italic text-lg font-semibold">us</Text> and sets the tone for fun. What name&apos;s calling your heart?
+                            </P>
 
-                                <View className="gap-y-2">
-                                    <Label nativeID="name">Name</Label>
-                                    <Input nativeID="name" onChangeText={handleChange("name")} onBlur={handleBlur("name")} value={values.name} />
-                                    <ErrorMessage name="name" />
-                                </View>
+                            <View className="gap-y-2">
+                                <Label nativeID="name">Name</Label>
+                                <Input nativeID="name" onChangeText={handleChange("name")} onBlur={handleBlur("name")} value={values.name} />
+                                <ErrorMessage name="name" />
                             </View>
+                        </View>
 
-                            <Button onPress={() => handleSubmit()}>
-                                <Text>Continue</Text>
-                            </Button>
-                        </>
-                    )}
-                </Formik>
-            </View>
+                        <Button onPress={() => handleSubmit()}>
+                            <Text>Continue</Text>
+                        </Button>
+                    </View>
+                )}
+            </Formik>
         </SafeAreaView>
     )
 }
--- app/onboarding/index.tsx
+++ app/onboarding/index.tsx
@@ -2,27 +2,28 @@ import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Button } from "~/components/ui/button";
 import { Text } from "~/components/ui/text";
-import { H1 } from "~/components/ui/typography";
+import { H1, P } from "~/components/ui/typography";
 import { Image } from "expo-image";
 import { router } from "expo-router";
 
 export default function OnboardingStepOne() {
     return (
-        <SafeAreaView className="px-6 flex-1 gap-y-16">
-            <View className="items-center mt-24 gap-y-4">
+        <SafeAreaView className="px-6 flex-1 gap-y-8">
+            <View className="items-center mt-12 gap-y-4">
                 <H1>Welcome to Nestly!</H1>
             </View>
 
             <Image
-                style={{ width: '100%', height: 300 }}
+                style={{ width: '100%', height: 200 }}
                 source={require("~/assets/images/onboarding/step-1.png")}
                 contentFit="contain"
             />
 
             <View className="flex-1 justify-between">
-                <Text>First you&apos;ll get to create a household. This will allow you to add members to your home and manage your household settings.</Text>
-
-                <Button onPress={() => router.push('/onboarding/household')}>
+                <P className="text-lg">
+                    {"Welcome to your new home command center! This app is your trusty sidekick for keeping life organized and stress-free. Buckle up for a quick setup, and let’s make your household shine brighter than a disco ball!"}
+                </P>
+                <Button onPress={() => router.navigate('/onboarding/household')}>
                     <Text>Continue</Text>
                 </Button>
             </View>
