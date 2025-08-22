20ecf02f44515bf86def9462db7ca9b117016e03
tomirons <tom.irons@hotmail.com>
2025-08-22 09:40:02 -0400

    remove tab bar styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,5 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
 import { byPrefixAndName } from '@awesome.me/kit-5314873f9e/icons'
 import { Colors } from '~/constants/Colors';
 import { useColorScheme } from '~/hooks/useColorScheme';
@@ -14,15 +13,6 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        // tabBarButton: HapticTab,
-        // tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
       }}>
       <Tabs.Screen
         name="mealplan"
