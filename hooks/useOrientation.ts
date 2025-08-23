3d5b5c756530172b29a1bc03baf886362c07190d
tomirons <tom.irons@hotmail.com>
2025-08-23 14:17:47 -0400

    create orientation hook

---
+++ hooks/useOrientation.ts
@@ -0,0 +1,11 @@
+import { useWindowDimensions } from "react-native";
+
+export function useOrientation() {
+  const { width, height } = useWindowDimensions();
+  const isLandscape = width > height;
+
+  return {
+    isLandscape,
+    isPortrait: !isLandscape
+  }
+}
