403197eb2bf5d6cb296acc0d025dc0c0fed51869
tomirons <tom.irons@hotmail.com>
2025-08-29 15:31:39 -0400

    use form sheet and set to full height

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -117,9 +117,9 @@ function Screens() {
           </Stack.Protected>
 
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="household/change" options={{ headerShown: false, presentation: 'modal' }} />
-          <Stack.Screen name="household/create" options={{ headerShown: false, presentation: 'modal' }} />
-          <Stack.Screen name="household/members" options={{ headerShown: false, presentation: 'formSheet' }} />
+          <Stack.Screen name="household/change" options={{ headerShown: false, presentation: 'formSheet', contentStyle: { height: '100%' } }} />
+          <Stack.Screen name="household/create" options={{ headerShown: false, presentation: 'formSheet', contentStyle: { height: '100%' } }} />
+          <Stack.Screen name="household/members" options={{ headerShown: false, presentation: 'formSheet', contentStyle: { height: '100%' } }} />
         </Stack.Protected>
 
         <Stack.Screen name="+not-found" />
