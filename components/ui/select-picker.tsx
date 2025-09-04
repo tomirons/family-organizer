28f9a796746661547239caf3f28993f39560fc0e
tomirons <tom.irons@hotmail.com>
2025-09-04 15:10:33 -0400

    create select picker

---
+++ components/ui/select-picker.tsx
@@ -0,0 +1,29 @@
+import { useColorScheme } from "~/lib/useColorScheme";
+import { useThemeColor } from "~/hooks/useThemeColor";
+import RNPickerSelect, { PickerSelectProps } from 'react-native-picker-select';
+import { inputClassName } from "~/components/ui/input";
+
+function SelectPicker({ placeholderLabel, ...props }: PickerSelectProps & { placeholderLabel?: string }) {
+    const { isDarkColorScheme } = useColorScheme();
+    const placeHolderColor = useThemeColor('muted-foreground');
+
+    return (
+        <RNPickerSelect
+            darkTheme={isDarkColorScheme}
+            placeholder={{
+                label: placeholderLabel ?? 'Select an option...',
+                value: undefined,
+                color: placeHolderColor
+            }}
+            textInputProps={{
+                className: inputClassName,
+            }}
+            style={{
+                inputIOSContainer: { pointerEvents: "none" }
+            }}
+            {...props}
+        />
+    )
+}
+
+export { SelectPicker };
