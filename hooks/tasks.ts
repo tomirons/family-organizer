651d71f06dac2a11ae18b563b7d24128a2e5371f
tomirons <tom.irons@hotmail.com>
2025-10-03 16:54:40 -0400

    add task hooks

---
+++ hooks/tasks.ts
@@ -0,0 +1,17 @@
+import { FormikValues } from "formik";
+import useSWR from "swr";
+import { useAuthenticationContext } from "~/contexts/authentication-context";
+import axios from "~/lib/axios";
+
+export const useLists = () => {
+    const { household } = useAuthenticationContext();
+    
+    return useSWR(
+        `households/${household?.id}/lists?include=tasks`,
+        (url) => axios.get(url).then((res) => res.data.data)
+    );
+};
+
+export const createTask = (household: string, list: string, values: FormikValues) => axios.post(`/households/${household}/lists/${list}/tasks`, values);
+
+export const updateTask = (household: string, list: string, id: string, values: FormikValues) => axios.put(`/households/${household}/lists/${list}/tasks/${id}`, values);
